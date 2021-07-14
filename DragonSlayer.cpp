1b19cf82fc7c3f74fe763be74a021d71c93d1c58
Stokely Diamantis <stokelydiamantis@gmail.com>
2021-07-14 23:38:13 +0000

    cleaned up initial errors

--- DragonSlayer.cpp
+++ DragonSlayer.cpp
@@ -1,6 +1,8 @@
 #include "DragonSlayer.h"
 #include "Dragon.h"
 
+#include <cassert>
+
 DragonSlayer::DragonSlayer(std::string name_, int hp_, int armor_) :
     Character(hp_, armor_, 4),
     name(name_)
