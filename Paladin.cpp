fb970cab23e6f7d3d10dbc9161f3a39ffccc71dd
Stokely Diamantis <stokelydiamantis@gmail.com>
2021-07-15 02:13:31 +0000

    tweaked values to match output

--- Paladin.cpp
+++ Paladin.cpp
@@ -5,8 +5,8 @@ Paladin::Paladin(std::string name_, int hp_, int armor_) :
     Character(hp_, armor_, 10),
     name(name_)
 {
-    helpfulItems = makeHelpfulItems(3);
-    defensiveItems = makeDefensiveItems(7);
+    helpfulItems = makeHelpfulItems(2);
+    defensiveItems = makeDefensiveItems(6);
 }
 
 const std::string& Paladin::getName()
--- Utility.cpp
+++ Utility.cpp
@@ -96,7 +96,6 @@ void useAttackItem(Character* character, Item* item)
     }
     else if( auto* dsch = dynamic_cast<DragonSlayer*>(character))
     {
-        // assert(false);
         //DragonSlayers get a 10x boost when attacking dragons, from their attack item.
         //so their attack item should boost their attack damage by a factor of 10
         //this means you need to GET the attack damage, multiply it by the item's boost, and BOOST the attackDamage with that multiplied value.  
